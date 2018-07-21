71b054f8ce250e66cecef468ec037e7370351235
Justin Menga <justin.menga@gmail.com>
2018-07-21 12:47:44 +1200

    Add support for Docker secrets

--- src/todobackend/settings_release.py
+++ src/todobackend/settings_release.py
@@ -4,8 +4,21 @@ import os
 # Disable debug
 DEBUG = True
 
+# Looks up secret in following order:
+#  1. /run/secret/<key>
+#  2. Environment variable named <key>
+#  3. Value of default or None if no default supplied
+def secret(key, default=None):
+    root = os.environ.get('SECRETS_ROOT','/run/secrets')
+    path = os.path.join(root,key)
+    if os.path.isfile(path):
+        with open(path) as f:
+            return f.read().rstrip()
+    else:
+        return os.environ.get(key,default)
+
 # Set secret key
-SECRET_KEY = os.environ.get('SECRET_KEY', SECRET_KEY)
+SECRET_KEY = secret('SECRET_KEY', SECRET_KEY)
 
 # Must be explicitly specified when Debug is disabled
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
@@ -16,7 +29,7 @@ DATABASES = {
         'ENGINE': 'mysql.connector.django',
         'NAME': os.environ.get('MYSQL_DATABASE','todobackend'),
         'USER': os.environ.get('MYSQL_USER','todo'),
-        'PASSWORD': os.environ.get('MYSQL_PASSWORD','password'),
+        'PASSWORD': secret('MYSQL_PASSWORD','password'),
         'HOST': os.environ.get('MYSQL_HOST','localhost'),
         'PORT': os.environ.get('MYSQL_PORT','3306'),
     },
